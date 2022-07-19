9594e316d4f7955d9be7ed1d054598a5c86c02a7
Aaron Ujah <aaronujah@gmail.com>
2022-07-19 11:28:13 +0100

    Initial commit of the StudyBud App
    Adding the whole project for the first time to Github. The project is now fully functional.

---
+++ base/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+# Register your models here.
+from .models import Rooms, Topic, Message, User
+
+admin.site.register(User)
+admin.site.register(Rooms)
+admin.site.register(Topic)
+admin.site.register(Message)
